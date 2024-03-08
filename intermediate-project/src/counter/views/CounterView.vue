6482a6273e1a52d9c6b8e5dece6b77a70af70929
Carlos Pérez Martín <cperezmrtn@gmail.com>
2024-03-08 13:58:21 +0100

    git init

---
+++ intermediate-project/src/counter/views/CounterView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">;
+import { ref } from 'vue';
+
+const counter = ref(5);
+const increase = (): void => {
+  ++counter.value;
+};
+</script>
+
+<template>
+  <h1>CounterView: {{ counter }}</h1>
+  <button @click="increase">+1</button>
+</template>;
